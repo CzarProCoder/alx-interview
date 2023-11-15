15b5dabea1c06b04a4defb303ba310dff1b02c6b
CzarProCoder <juliusczar36@gmail.com>
2023-11-15 16:09:07 +0300

    Create a function to rotate a matrix by 90 degree

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -6,4 +6,15 @@ Module that rotates a 2D matrix in a 90 degrees
 
 
 def rotate_2d_matrix(matrix):
-    pass
+    '''
+    Rotate a 2d matrix
+    '''
+    n = len(matrix)
+
+    for i in range(n):
+        for j in range(i + 1, n):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+    for row in matrix:
+        row.reverse()
+
+    return matrix
