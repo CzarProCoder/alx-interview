b72198852d96320c4b157e8f80927d63aa2b72ed
CzarProCoder <juliusczar36@gmail.com>
2023-11-23 20:52:40 +0300

    Update code as per the pythonic way

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -7,7 +7,7 @@ Module to calculate the least number of coins required to meet total
 
 def makeChange(coins, total):
     '''
-    Determine the fewest number of coins required to 
+    Determine the fewest number of coins required to
     achieve the passed total
     '''
     if total < 0:
@@ -17,4 +17,3 @@ def makeChange(coins, total):
         for i in range(coin, total + 1):
             dp[i] = min(dp[i], dp[i - coin] + 1)
     return dp[-1] if dp[-1] != float("inf") else -1
-    
